b148b9b8aaa02d065d3188096e8a54513d6a23b5
hiiii5 <dustinroden_Games@ymail.com>
2014-01-17 15:45:54 -0600

    Update DungeonMaster.js
    Added in the tiled map.

--- DungeonMaster.js
+++ DungeonMaster.js
@@ -7,6 +7,10 @@ canvasElement.width = width;
 canvasElement.height = height;
 var ctx = canvasElement;
 
+var map = [];
+var cellWidth = 5;
+var cellHeight = 5;
+
 if(canvasElement.getContext)
 {
 	ctx = canvasElement.getContext("2d");
@@ -18,22 +22,47 @@ if(canvasElement.getContext)
 }
 else { alert("Your browser doesn't support <canvas>."); }
 
-function update()
+function createWorld(x, y)
 {
+	for(var i=0; i<x; i++)
+	{
+		map[i] = [];
+		for(var j=0; j<y; j++)
+		{
+			map[i][j] = Math.round(Math.random()*1);
+			console.log("val " + j + ": " + map[i][j]);
+		}
+	}
 }
 
+createWorld(50, 50);
+
+function update()
+{
+	
+}
 
 function draw()
 {
 	ctx.clearRect(0, 0, width, height);
+	
+	for(var i=0; i<map.length; i++)
+	{
+		for(var j=0; j<map[i].length; j++)
+		{
+			if(map[i][j] == 0) { ctx.fillStyle = "white"; }
+			else if(map[i][j] == 1) { ctx.fillStyle = "black"; }
+			ctx.fillRect(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
+		}
+	}
 }
 
 function getClick(e)
 {
 	var evt = e || window.event;
-	var mx = evt.pageX;
-	var my = evt.pageY;
+	mx = evt.pageX;
+	my = evt.pageY;
 	clicked = true;
 }
 
-canvasElement.addEventListner("mousedown", getClick);
+canvasElement.addEventListener("mousedown", getClick);
