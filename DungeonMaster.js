ba32aacf842743d99e2c38b9c9d09be9ebcf0de5
keltk <keltik05@gmail.com>
2014-01-20 11:14:29 -0600

    Update DungeonMaster.js
    added the ability to change the clicked location to green. Check Code for a more detailed description

--- DungeonMaster.js
+++ DungeonMaster.js
@@ -8,8 +8,8 @@ canvasElement.height = height;
 var ctx = canvasElement;
 
 var map = [];
-var cellWidth = 5;
-var cellHeight = 5;
+var cellWidth = 16;//made the cells fit the canvas
+var cellHeight = 12;
 
 if(canvasElement.getContext)
 {
@@ -52,17 +52,35 @@ function draw()
 		{
 			if(map[i][j] == 0) { ctx.fillStyle = "white"; }
 			else if(map[i][j] == 1) { ctx.fillStyle = "black"; }
+			else if(map[i][j] == 3) { ctx.fillStyle = "green"; }//added green to tell where i clicked
 			ctx.fillRect(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
 		}
 	}
+}
+function getMapX(pos){
+	for(var i=0; i<map.length; i++){
+		if ( pos >= i*cellWidth && pos <= (i+1)*cellWidth ){
+			return i;
+		}
+	}
+	return;
+}
+function getMapY(pos){
+	for(var j=0; j<map.length; j++){
+		if ( pos >= j*cellHeight && pos <= (j+1)*cellHeight ){
+			return j;
+		}
+	}
+	return;
 }
 
 function getClick(e)
 {
 	var evt = e || window.event;
-	mx = evt.pageX;
-	my = evt.pageY;
+	mx = evt.pageX - 8;//these needed to be offset it's checking the page x and y not the canvas
+	my = evt.pageY - 8;
 	clicked = true;
+	map[getMapX(mx)][getMapY(my)] = 3;//sets where you clicked to 3 "green"
 }
 
 canvasElement.addEventListener("mousedown", getClick);
