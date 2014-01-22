b5ff9ff2fd586d09a2556a4122d6dab6189bcaf5
hiiii5 <dustinroden_Games@ymail.com>
2014-01-22 15:46:07 -0600

    Update DungeonMaster.js
    Changelog: v0.0.3
    =========================
    - Added Start Area.
    - Blacked out all but start area.
    - Added Mouse drag and kept mouse click.
    - Removed Unnecessary Drawing.
    =========================
    TODO:
    =========================
    - Fix offset issue.
    - Fix error off canvas.

--- DungeonMaster.js
+++ DungeonMaster.js
@@ -1,15 +1,17 @@
-var width = 800;
-var height = 600;
+var width;
+var height;
 var FPS = 60;
 
 var canvasElement = document.getElementById('canvas');
-canvasElement.width = width;
-canvasElement.height = height;
 var ctx = canvasElement;
 
 var map = [];
-var cellWidth = 16;//made the cells fit the canvas
-var cellHeight = 12;
+var cellWidth = 15;//made the cells fit the canvas
+var cellHeight = 15;
+var HQSize = 4;
+var clicked = false;
+var mx = 0;
+var my = 0;
 
 if(canvasElement.getContext)
 {
@@ -29,17 +31,32 @@ function createWorld(x, y)
 		map[i] = [];
 		for(var j=0; j<y; j++)
 		{
-			map[i][j] = Math.round(Math.random()*1);
-			console.log("val " + j + ": " + map[i][j]);
+			//Start Room
+			if((i>=(x/2)-(HQSize/2) && i<=(x/2)+(HQSize/2)) && (j>=(y/2)-(HQSize/2) && j<=(y/2)+(HQSize/2)))
+				map[i][j] = 0;
+			else //Blackness
+				map[i][j] = 1;
 		}
 	}
 }
 
 createWorld(50, 50);
 
+width = map.length * cellWidth;
+height = map[0].length * cellHeight;
+
+canvasElement.width = width;
+canvasElement.height = height;
+
 function update()
 {
-	
+	if(clicked == true)
+	{
+		if(map[getMapX(mx)][getMapY(my)] > 0)
+		{
+			map[getMapX(mx)][getMapY(my)] = 2
+		}
+	}
 }
 
 function draw()
@@ -52,7 +69,7 @@ function draw()
 		{
 			if(map[i][j] == 0) { ctx.fillStyle = "white"; }
 			else if(map[i][j] == 1) { ctx.fillStyle = "black"; }
-			else if(map[i][j] == 3) { ctx.fillStyle = "green"; }//added green to tell where i clicked
+			else if(map[i][j] == 2) { ctx.fillStyle = "green"; }//added green to tell where I clicked
 			ctx.fillRect(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
 		}
 	}
@@ -74,13 +91,24 @@ function getMapY(pos){
 	return;
 }
 
+function getPOS(e)
+{
+	var evt = e || window.event;
+	mx = evt.pageX - 8;//these needed to be offset it's checking the page x and y not the canvas
+	my = evt.pageY - 8;
+}
 function getClick(e)
 {
 	var evt = e || window.event;
 	mx = evt.pageX - 8;//these needed to be offset it's checking the page x and y not the canvas
 	my = evt.pageY - 8;
 	clicked = true;
-	map[getMapX(mx)][getMapY(my)] = 3;//sets where you clicked to 3 "green"
+}
+function stopClick()
+{
+	clicked = false;
 }
 
+canvasElement.addEventListener("mousemove", getPOS);
 canvasElement.addEventListener("mousedown", getClick);
+canvasElement.addEventListener("mouseup", stopClick);
