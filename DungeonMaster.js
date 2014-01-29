6dd184ed02b250791def5287d95efc1d411f6a9f
hiiii5 <dustinroden_Games@ymail.com>
2014-01-29 14:49:57 -0600

    Update DungeonMaster.js
    Added types and cell object with values

--- DungeonMaster.js
+++ DungeonMaster.js
@@ -15,29 +15,43 @@ var my = 0;
 
 if(canvasElement.getContext)
 {
-	ctx = canvasElement.getContext("2d");
+        ctx = canvasElement.getContext("2d");
 
-	setInterval(function() {
-		update();
-		draw();
-	}, 1000/FPS);
+        setInterval(function() {
+                update();
+                draw();
+        }, 1000/FPS);
 }
 else { alert("Your browser doesn't support <canvas>."); }
 
+function Cell(x, y, type, collide)
+{
+	var b = {};
+
+	b.x = x;
+	b.y = y;
+	b.width= cellWidth;
+	b.height = cellHeight;
+	b.type = type;
+	b.collide = collide;
+
+	return b;
+}
+
 function createWorld(x, y)
 {
-	for(var i=0; i<x; i++)
-	{
-		map[i] = [];
-		for(var j=0; j<y; j++)
-		{
-			//Start Room
-			if((i>=(x/2)-(HQSize/2) && i<=(x/2)+(HQSize/2)) && (j>=(y/2)-(HQSize/2) && j<=(y/2)+(HQSize/2)))
-				map[i][j] = 0;
-			else //Blackness
-				map[i][j] = 1;
-		}
-	}
+        for(var i=0; i<x; i++)
+        {
+                map[i] = [];
+                for(var j=0; j<y; j++)
+                {
+                        //Start Room
+                        if((i>=(x/2)-(HQSize/2) && i<=(x/2)+(HQSize/2)) && (j>=(y/2)-(HQSize/2) && j<=(y/2)+(HQSize/2)))
+                                map[i][j] = new Cell(i, j, 0, false);
+                        else //Blackness
+                                map[i][j] = new Cell(i, j, 1, true);
+                }
+        }
 }
 
 createWorld(50, 50);
@@ -50,63 +64,70 @@ canvasElement.height = height;
 
 function update()
 {
-	if(clicked == true)
-	{
-		if(map[getMapX(mx)][getMapY(my)] > 0)
-		{
-			map[getMapX(mx)][getMapY(my)] = 2
-		}
-	}
+        if(clicked == true)
+        {
+                if(map[getMapX(mx)][getMapY(my)].type > 0)
+                {
+                        map[getMapX(mx)][getMapY(my)].type = 2
+                }
+        }
 }
 
 function draw()
 {
-	ctx.clearRect(0, 0, width, height);
-	
-	for(var i=0; i<map.length; i++)
-	{
-		for(var j=0; j<map[i].length; j++)
+        ctx.clearRect(0, 0, width, height);
+        
+        for(var i=0; i<map.length; i++)
+        {
+                for(var j=0; j<map[i].length; j++)
+                {
+                        if(map[i][j].type == 0) { ctx.fillStyle = "white"; }
+                        else if(map[i][j].type == 1) { ctx.fillStyle = "black"; }
+                        else if(map[i][j].type == 2) { ctx.fillStyle = "green"; }//added green to tell where I clicked
+                        ctx.fillRect(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
+                }
+        }
+}
+function getMapX(pos)
+{
+        for(var i=0; i<map.length; i++)
 		{
-			if(map[i][j] == 0) { ctx.fillStyle = "white"; }
-			else if(map[i][j] == 1) { ctx.fillStyle = "black"; }
-			else if(map[i][j] == 2) { ctx.fillStyle = "green"; }//added green to tell where I clicked
-			ctx.fillRect(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
-		}
-	}
+                if ( pos >= i*cellWidth && pos <= (i+1)*cellWidth )
+				{
+                        return i;
+                }
+        }
+        return;
 }
-function getMapX(pos){
-	for(var i=0; i<map.length; i++){
-		if ( pos >= i*cellWidth && pos <= (i+1)*cellWidth ){
-			return i;
-		}
-	}
-	return;
-}
-function getMapY(pos){
-	for(var j=0; j<map.length; j++){
-		if ( pos >= j*cellHeight && pos <= (j+1)*cellHeight ){
-			return j;
-		}
-	}
-	return;
+
+function getMapY(pos)
+{
+        for(var j=0; j<map.length; j++)
+		{
+                if ( pos >= j*cellHeight && pos <= (j+1)*cellHeight )
+				{
+                        return j;
+                }
+        }
+        return;
 }
 
 function getPOS(e)
 {
-	var evt = e || window.event;
-	mx = evt.pageX - 8;//these needed to be offset it's checking the page x and y not the canvas
-	my = evt.pageY - 8;
+        var evt = e || window.event;
+        mx = evt.pageX - 8;//these needed to be offset it's checking the page x and y not the canvas
+        my = evt.pageY - 8;
 }
 function getClick(e)
 {
-	var evt = e || window.event;
-	mx = evt.pageX - 8;//these needed to be offset it's checking the page x and y not the canvas
-	my = evt.pageY - 8;
-	clicked = true;
+        var evt = e || window.event;
+        mx = evt.pageX - 8;//these needed to be offset it's checking the page x and y not the canvas
+        my = evt.pageY - 8;
+        clicked = true;
 }
 function stopClick()
 {
-	clicked = false;
+        clicked = false;
 }
 
 canvasElement.addEventListener("mousemove", getPOS);
