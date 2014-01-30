5ce7af6042845632be236e08d451810df28a9b76
hiiii5 <dustinroden_Games@ymail.com>
2014-01-30 15:17:46 -0600

    Update DungeonMaster.js
    Added Menu.

--- DungeonMaster.js
+++ DungeonMaster.js
@@ -6,6 +6,8 @@ var canvasElement = document.getElementById('canvas');
 var ctx = canvasElement;
 
 var map = [];
+var menuBar = [];
+var menuBarText = [];
 var cellWidth = 15;//made the cells fit the canvas
 var cellHeight = 15;
 var HQSize = 4;
@@ -23,6 +25,30 @@ if(canvasElement.getContext)
     }, 1000/FPS);
 }
 else { alert("Your browser doesn't support <canvas>."); }
+
+function Menu(x, y, width, height, items)
+{
+	var list = {};
+	
+	list.x = x;
+	list.y = y;
+	list.width = width;
+	list.height = height;
+	list.items = items;
+	
+	list.changeItem = function(index, value)
+	{
+		items[index] = value;
+	}
+	
+	return list;
+}
+
+function createMenu()
+{
+	menuBar[0] = new Menu(0, width, height-200, 200, ["Construction"]);
+	menuBar[0][0] = new Menu(10, 170, height-170, 130, ["Traps", "Monsters", "Building", "Sell", "Back"]);
+}
 
 function Cell(x, y, type, collide)
 {
@@ -62,14 +88,20 @@ height = map[0].length * cellHeight + 200;
 canvasElement.width = width;
 canvasElement.height = height;
 
+createMenu();
+menuBarText[0] = menuBar[0].items[0];
+
 function update()
 {
     if(clicked == true)
     {
-        if(map[getMapX(mx)][getMapY(my)].type > 0)
-        {
-            map[getMapX(mx)][getMapY(my)].type = 2;
-        }
+		if(my < height-200)
+		{
+			if(map[getMapX(mx)][getMapY(my)].type > 0)
+			{
+				map[getMapX(mx)][getMapY(my)].type = 2;
+			}
+		}
     }
 }
 
@@ -77,6 +109,14 @@ function draw()
 {
     ctx.clearRect(0, 0, width, height);
     
+	ctx.fillStyle = "blue";
+	ctx.fillRect(menuBar[0].x, menuBar[0].y, menuBar[0].width, menuBar[0].height);
+	ctx.fillStyle = "black";
+	ctx.font="20px Consolas";
+
+	for(var i=0; i<menuBar[0][0].items.length; i++)
+		ctx.fillText(menuBarText[i], menuBar[0].x+20 + (150)*i, menuBar[0].y+100);
+	
     for(var i=0; i<map.length; i++)
     {
         for(var j=0; j<map[i].length; j++)
@@ -94,8 +134,7 @@ function getMapX(pos)
 	{
         if ( pos >= i*cellWidth && pos <= (i+1)*cellWidth )
 		{
-			if(pos < height - 200)
-                return i;
+            return i;
         }
     }
     return;
@@ -103,13 +142,16 @@ function getMapX(pos)
 
 function getMapY(pos)
 {
-    for(var j=0; j<map.length; j++)
+	if(pos < height - 200)
 	{
-        if ( pos >= j*cellHeight && pos <= (j+1)*cellHeight )
+		for(var j=0; j<map.length; j++)
 		{
-            return j;
-        }
-    }
+			if ( pos >= j*cellHeight && pos <= (j+1)*cellHeight )
+			{
+				return j;
+			}
+		}
+	}
     return;
 }
 
@@ -124,6 +166,20 @@ function getClick(e)
     var evt = e || window.event;
     mx = evt.pageX - ctx.canvas.offsetLeft;
     my = evt.pageY - ctx.canvas.offsetTop;
+
+	console.log(menuBar[0].x + ", " + menuBar[0].y + ", " + menuBar[0].width + ", " + menuBar[0].height);
+	if(mx > menuBar[0].x && mx < menuBar[0].x + menuBar[0].width && my > menuBar[0].y && my < menuBar[0].y + menuBar[0].height)
+	{
+		if(mx > menuBar[0].x && mx < menuBar[0].x + 150 && my > menuBar[0].y && my < menuBar[0].y+200)
+		{
+			for(var i=0; i<menuBar[0][0].items.length; i++)
+			{
+				console.log(menuBar[0][0].items.length + ", " + menuBar[0][0].items[i] + ", " + menuBarText[i]);
+				menuBarText[i] = menuBar[0][0].items[i];
+			}
+		}
+	}
+
     clicked = true;
 }
 function stopClick()
